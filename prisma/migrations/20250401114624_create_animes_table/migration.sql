4bf5356be617febf6c55a0527ca916e0f78c2099
Matheus Couto <158165513+MatheusCouto8@users.noreply.github.com>
2025-04-01 08:48:05 -0300

    feat: create animes table with prisma

---
+++ prisma/migrations/20250401114624_create_animes_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "animes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "episodes" INTEGER NOT NULL,
+    "studio" TEXT NOT NULL,
+    "genres" TEXT NOT NULL,
+    "rating" REAL NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
